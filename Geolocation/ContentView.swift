63716581c96a35931873b224c7d8271e8976c64d
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2023-03-05 20:51:13 +0100

    Add bottom text container for data

--- Geolocation/ContentView.swift
+++ Geolocation/ContentView.swift
@@ -12,12 +12,19 @@ struct ContentView: View {
     @StateObject private var viewModel = ContentViewModel()
     
     var body: some View {
-        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
-            .ignoresSafeArea()
-            .accentColor(Color(.systemPink))
-            .onAppear {
-                viewModel.checkIfLocationServicesIsEnabled()
-            }
+        VStack {
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+                .ignoresSafeArea()
+                .accentColor(Color(.systemPink))
+                .onAppear {
+                    viewModel.checkIfLocationServicesIsEnabled()
+                }
+            VStack {
+                Text("km/h")
+                Text("lat: ")
+                Text("lon: ")
+            }.padding()
+        }
     }
 }
 
