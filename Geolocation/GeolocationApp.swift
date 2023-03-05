125692b371f5e657effdeb088aa5d6f5f518f441
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2023-03-05 16:22:07 +0100

    Initial Commit

---
+++ Geolocation/GeolocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeolocationApp.swift
+//  Geolocation
+//
+//  Created by Ferran Opticks on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GeolocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
